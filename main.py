95d2c331262868c6119baa590f6fe3a57d8ed308
TeslaCast <159522080+TeslaCast@users.noreply.github.com>
2025-03-21 02:22:53 +0300

    main.py
    api skeleton added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from utils import *
+from fastapi import FastAPI
+import os
+from typing import Optional
 
+app = FastAPI()
 
-# Пример использования
-
-
-    
+@app.get("/")
+def get_game_info():
+    return out_res()
